72154951028d68fb2f533f4ec9ab6f997ee7a383
Francis Frias <francisr.frias@gmail.com>
2025-02-04 07:10:21 +0800

    email form done

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,29 +1,46 @@
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import * as z from 'zod';
 import { Button } from './ui/Button';
 import { Input } from './ui/Input';
 import Navbar from './ui/Navbar';
 import ringpattern from '/images/pattern-rings.svg';
 
-// const messageForm = z.object({
-//   name: z
-//     .string({
-//       required_error: 'Sorry, name is required',
-//     })
-//     .min(2, {
-//       message: 'Sorry, name should be more than 2 letters',
-//     }),
-//   email: z.string().email({
-//     message: 'Sorry, invalid email format here',
-//   }),
-//   message: z.string().optional(),
-// });
+const messageForm = z.object({
+  name: z
+    .string({
+      required_error: 'Sorry, name is required',
+    })
+    .min(2, {
+      message: 'Sorry, name should be more than 2 letters',
+    }),
+  email: z.string().email({
+    message: 'Sorry, invalid email format here',
+  }),
+  message: z.string().optional(),
+});
 
-// const messageInitialState = {
-//   name: '',
-//   email: '',
-//   message: '',
-// };
+type messageInterface = z.infer<typeof messageForm>;
+
+const messageInitialState = {
+  name: '',
+  email: '',
+  message: '',
+};
 
 const Footer = () => {
+  const form = useForm({
+    mode: 'onChange',
+    resolver: zodResolver(messageForm),
+    defaultValues: messageInitialState,
+  });
+
+  const { errors } = form.formState;
+
+  const handleSubmit = (data: messageInterface) => {
+    console.log(data);
+  };
+
   return (
     <footer className='bottom-0 w-full text-center py-[60px] bg-[#242424] text-primary  relative px-4 md:pb-4'>
       <div className='flex flex-col gap-5 md:px-[146px] '>
@@ -40,15 +57,30 @@ const Footer = () => {
             fill in the form, and I’ll get back to you as soon as possible.
           </p>
         </div>
-        <div className='flex flex-col gap-5 '>
-          <Input placeholder='Name' />
-          <Input placeholder='Email' />
-          <textarea
-            placeholder='Message'
-            className='min-h-[107px] border-b-2 border-white w-full bg-transparent p-2 uppercase placeholder:uppercase placeholder:pb-20 placeholder:border-red-300 placeholder:border-2 placeholder:tracking-[-0.22px] placeholder:leading-[26px] placeholder:text-[16px] placeholder:opacity-50 placeholder:text-secondary placeholder:font-medium placeholder:pl-3 ring-0 focus:ring-0 focus:outline-none focus:border-accent focus:border-b-2 text-primary text-[16px] leading-[26px] font-medium required:border-b-2 required:border-error disabled:border-b-2 disabled:border-error no-scrollbar'
-          />
-          <Button className='w-fit block ml-auto '>Send Message</Button>
-        </div>
+
+        <form {...form} onSubmit={form.handleSubmit(handleSubmit)}>
+          <div className='flex flex-col gap-8 '>
+            <Input
+              placeholder='Name'
+              error={errors.name}
+              {...form.register('name')}
+            />
+            <Input
+              placeholder='Email'
+              error={errors.email}
+              {...form.register('email')}
+            />
+            <textarea
+              placeholder='Message'
+              className='min-h-[107px] border-b-2 border-white w-full bg-transparent p-2 uppercase placeholder:uppercase placeholder:pb-20 placeholder:border-red-300 placeholder:border-2 placeholder:tracking-[-0.22px] placeholder:leading-[26px] placeholder:text-[16px] placeholder:opacity-50 placeholder:text-secondary placeholder:font-medium placeholder:pl-3 ring-0 focus:ring-0 focus:outline-none focus:border-accent focus:border-b-2 text-primary text-[16px] leading-[26px] font-medium required:border-b-2 required:border-error disabled:border-b-2 disabled:border-error no-scrollbar'
+              {...form.register('message')}
+            />
+
+            <Button className='w-fit block ml-auto ' type='submit'>
+              Send Message
+            </Button>
+          </div>
+        </form>
       </div>
       <div className='w-full border-t-2 border-primary mt-[68px] pt-9 md:mt-[105px] md:pt-[30px]'>
         <Navbar />
--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -1,22 +1,39 @@
 import * as React from 'react';
+import { FieldError } from 'react-hook-form';
+import errorIcon from '../../../public/images/errorsvg.svg';
 import { cn } from '../../helper/utils';
 
-const Input = React.forwardRef<
-  HTMLInputElement,
-  React.InputHTMLAttributes<HTMLInputElement>
->(({ className, type, ...props }, ref) => {
-  return (
-    <input
-      type={type}
-      className={cn(
-        'min-h-[43px] border-b-2 border-white w-full bg-transparent p-2 uppercase placeholder:uppercase placeholder:pb-20 placeholder:border-red-300 placeholder:border-2 placeholder:tracking-[-0.22px] placeholder:leading-[26px] placeholder:text-[16px] placeholder:opacity-50 placeholder:text-secondary placeholder:font-medium placeholder:pl-3 ring-0 focus:ring-0 focus:outline-none focus:border-accent focus:border-b-2 text-primary text-[16px] leading-[26px] font-medium required:border-b-2 required:border-error disabled:border-b-2 disabled:border-error  ',
-        className
-      )}
-      ref={ref}
-      {...props}
-    />
-  );
-});
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  error?: FieldError | undefined;
+}
+
+const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ className, type, error, ...props }, ref) => {
+    return (
+      <div className='relative'>
+        <input
+          aria-label='input'
+          type={type}
+          className={cn(
+            'min-h-[43px] border-b-2 border-white w-full bg-transparent p-2 uppercase placeholder:uppercase placeholder:pb-20 placeholder:border-red-300 placeholder:border-2 placeholder:tracking-[-0.22px] placeholder:leading-[26px] placeholder:text-[16px] placeholder:opacity-50 placeholder:text-secondary placeholder:font-medium placeholder:pl-3 ring-0 focus:ring-0 focus:outline-none focus:border-accent focus:border-b-2 text-primary text-[16px] leading-[26px] font-medium required:border-b-2 required:border-error disabled:border-b-2 disabled:border-error  ',
+            error?.message && 'border-error',
+            className
+          )}
+          ref={ref}
+          {...props}
+        />
+        {error?.message && (
+          <>
+            <p className='text-error text-[12px] text-medium leading-4 tracking-[-0.17px] text-right mt-1.5'>
+              {error?.message}
+            </p>
+            <img src={errorIcon} className='absolute right-0 top-0 ' />
+          </>
+        )}
+      </div>
+    );
+  }
+);
 Input.displayName = 'Input';
 
 export { Input };
